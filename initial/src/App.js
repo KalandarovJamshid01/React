d25454ad8f5e5466b5612e1eb98805c6e8303163
Jamshid <kalandarovjamshid01@gmail.com>
2022-09-28 11:29:24 +0500

    okay

--- initial/src/App.js
+++ initial/src/App.js
@@ -1,33 +1,9 @@
 import React, { useState } from "react";
 
 function App() {
-  const [toggleBtn, setToggleBtn] = useState(false);
-  const [value, setValue] = useState("sam");
   return (
     <>
-      <div className="app w-50 mx-auto border-1 shadow h-25">
-        <button
-          onClick={() => {
-            setToggleBtn(!toggleBtn);
-          }}
-          className="btn btn-success "
-        >
-          Toggle btn
-        </button>
-        {toggleBtn ? (
-          <p className="lead text-primary">YouTube Content</p>
-        ) : (
-          <p className="lead text-danger">YouTube Content</p>
-        )}
-
-        <p>Value :{value}</p>
-        <input
-          type="text"
-          className="form-Control"
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
-        />
-      </div>
+     
     </>
   );
 }
